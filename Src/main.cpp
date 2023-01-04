fd18ff8deb6757d0c005fc349643027edb071287
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2023-01-05 02:00:23 +0300

    feat: test new functionality

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,9 +1,14 @@
 #include <cstdlib>
-#include "ArrayPool.hpp"
 #include <iostream>
+
+#include "ArrayPool.hpp"
+#include "MemoryOwnerFactory.hpp"
 
 int main()
 {
+	std::cout << "Start press enter" << std::endl;
+	std::cin.get();
+	//Usage example 1
 	{
 		auto pool = ArrayPool::ArrayPool<int>();
 		int realSize = 0;
@@ -22,6 +27,42 @@ int main()
 
 		std::cout << "End loop" << std::endl;
 	}
+	std::cin.get();
+
+	//Usage example 2
+	{
+		auto pool = std::make_shared<ArrayPool::ArrayPool<int>>();
+		ArrayPool::MemoryOwnerFactory<int> memoryFactory(std::move(pool));
+
+		std::cout << "Start 'rentMemory' Loop 2000000" << std::endl;
+		for (int i = 0; i < 200000; i++)
+		{
+			ArrayPool::MemoryOwner<int> arr = memoryFactory.rentMemory(10);
+			ArrayPool::MemoryOwner<int> arr2 = memoryFactory.rentMemory(8);
+		}//return memory to pool in destructor automatically
+
+		std::cout << "End 'rentMemory' Loop" << std::endl;
+	}
+
+	//Usage example 2
+	{
+		auto pool = std::make_shared<ArrayPool::ArrayPool<int>>();
+		ArrayPool::MemoryOwnerFactory<int> memoryFactory(std::move(pool));
+
+		std::cout << "Start 'rentMemory' Loop 2000000" << std::endl;
+		for (int i = 0; i < 200000; i++)
+		{
+			ArrayPool::MemoryOwner<int>* arr = memoryFactory.newRentMemory(10);
+			ArrayPool::MemoryOwner<int>* arr2 = memoryFactory.newRentMemory(8);
+
+			//return memory to pool manual
+			delete arr;
+			delete arr2;
+		}
+
+		std::cout << "End 'rentMemory' Loop" << std::endl;
+	}
+
 	std::cin.get();
 
 	return EXIT_SUCCESS;
